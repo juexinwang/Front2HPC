4fc9bd4e82457522d9fdd35e7762151bc6882317
juexinwang <juexinwang@hotmail.com>
2022-12-13 13:30:30 -0500

    fix a bug for long string

--- BackJobsRunner.py
+++ BackJobsRunner.py
@@ -75,12 +75,12 @@ class BackJobsRunner:
         fileStr = fileStr + 'srun python main.py'
         ## Add params of main.py from input
         #inputdir: /N/u/soicwang/BigRed200/inputPDBDir/1213AAAA/data/
-        fileStr = fileStr + ' --jobid ' + self.jobid\
-        + ' --inputdir ' + self.inputHPCDir + self.jobid +'/data/'\ 
-        + ' --num-residues ' + num_residues\
-        + ' --timesteps ' + str(self.params['timestep_size'])\
-        + ' --number-expstart ' + str(self.params['start'])\
-        + ' --number-exp ' + str(self.params['end'])\
+        fileStr = fileStr + ' --jobid ' + self.jobid \
+        + ' --inputdir ' + self.inputHPCDir + self.jobid +'/'+'data/' \ 
+        + ' --num-residues ' + num_residues \
+        + ' --timesteps ' + str(self.params['timestep_size']) \
+        + ' --number-expstart ' + str(self.params['start']) \
+        + ' --number-exp ' + str(self.params['end']) \
         ## Add params of main.py
 
         fileStr = fileStr + ' --seed ' + str(self.params['seed'])\
