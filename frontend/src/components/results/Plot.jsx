fbbf37064641979cd0e2948dcc882fb164a3084f
juexinwang <juexinwang@hotmail.com>
2022-12-27 10:24:27 -0500

    update debug

--- frontend/src/components/results/Plot.jsx
+++ frontend/src/components/results/Plot.jsx
@@ -36,7 +36,7 @@ export default function Plot(props){
     }else{
       setExample(false)
     }
-    // console.log('aaaaa',props.results.imgs.edges_domain)
+    console.log('aaaaa',props.results.strucFilePath)
   },[props.results])
 
   
