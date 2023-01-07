d301d6f0cbf41a4c10143e00b61f026cfc354c60
juexinwang <juexinwang@hotmail.com>
2023-01-06 20:55:11 -0500

    update Carbonate for NRI-MD

--- HPC_NRI-MD_check_Carbonate.sh
+++ HPC_NRI-MD_check_Carbonate.sh
@@ -1,7 +1,7 @@
 ## Put this into the HPC: Carbonate as the backup
 
 # Check status
-sacct -S 2023-02-04 -u soicwang > status_current.txt
+sacct -S 2023-01-06 -u soicwang > status_current.txt
 diff status_old.txt status_current.txt > diff.txt
 # Read and dealwith diff.txt
 python3 /N/u/soicwang/Carbonate/projects/Front2HPC/HPC_NRI-MD_prepareTransfer_Carbonate.py
