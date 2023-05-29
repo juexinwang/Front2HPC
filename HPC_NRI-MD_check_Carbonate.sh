84669f45c869b10853606f8882e39d446441cb9d
juexinwang <juexinwang@hotmail.com>
2023-05-29 11:44:07 -0400

    automatically update the check time as yesterday

--- HPC_NRI-MD_check_Carbonate.sh
+++ HPC_NRI-MD_check_Carbonate.sh
@@ -1,7 +1,9 @@
 ## Put this into the HPC: Carbonate as the backup
 
-# Check status
-sacct -S 2023-01-06 -u soicwang > status_current.txt
+# Check status from yesterday
+ttime=$(date -d "yesterday 13:00 " '+%Y-%m-%d')
+sacct -S $ttime -u soicwang > status_current.txt
+# sacct -S 2023-04-06 -u soicwang > status_current.txt
 diff status_old.txt status_current.txt > diff.txt
 # Read and dealwith diff.txt
 python3 /N/u/soicwang/Carbonate/projects/Front2HPC/HPC_NRI-MD_prepareTransfer_Carbonate.py
